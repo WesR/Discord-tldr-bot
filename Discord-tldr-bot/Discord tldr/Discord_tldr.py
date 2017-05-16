0628bfd9b4893bcbcf77b5c6703d464e5fdd8d7d
Wes Ring <wring436@gmail.com>
2017-05-16 03:04:18 -0400

    Planning for tomarrow

--- Discord-tldr-bot/Discord tldr/Discord_tldr.py
+++ Discord-tldr-bot/Discord tldr/Discord_tldr.py
@@ -10,18 +10,25 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    
+    #TODO: message.contains
+
     if message.content.startswith('http://') or message.content.startswith('https://'):
         await client.send_message(message.channel, "Im reading, give me a second")
+        #TODO: Sanitize this
+        #TODO: Write own html scraper
         article = Article(message.content)
         article.download()
         article.parse()
+
+        #TODO: Write own summary function
         summary = "".join(Summarize(article.title, article.text))
         await client.send_message(message.channel, "\n\nSummary:\n " + summary)
 
 @client.event
 async def on_ready():
     '''
-    #I need a way to update it all
+    #I need a way to update everything
     if config.get("update","nltk") == "true":
         print("Updating nltk")
         #nltk.
