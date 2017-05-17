94af0e0ac33c4a4c822ef9a270f23eab7e7fd9eb
Wes Ring <wring436@gmail.com>
2017-05-17 02:25:18 -0400

    command framework and Blacklist command

--- Discord-tldr-bot/Discord tldr/Discord_tldr.py
+++ Discord-tldr-bot/Discord tldr/Discord_tldr.py
@@ -5,9 +5,7 @@ import configparser
 from pyteaser import Summarize
 from newspaper import Article
 
-config = configparser.ConfigParser()
 client = discord.Client()
-blacklistFile = open("blacklist", "r+")
 
 '''
     -autogenerate config file
@@ -26,15 +24,39 @@ blacklistFile = open("blacklist", "r+")
 @client.event
 async def on_message(message):
     
+    if message.content.startswith('<@' + client.user.id + ">"):
+        command = message.content.split('<@' + client.user.id + ">")[1].strip().rstrip()
+        if command[0:len('blacklist')] == 'blacklist':#Command: Blacklist
+            blacklistURL = command[len('blacklist'):len(command)].strip()#Remove the blacklist command
+            if blacklistURL.find("://") != -1:#If its not a clean url, remove the http and trailing address
+                blacklistURL = blacklistURL[(blacklistURL.find("://")+3):blacklistURL.find("/",blacklistURL.find("://")+3)]#Find whats inbetween :// and the next /
+
+            if blacklistURL.find(".") != blacklistURL.rfind("."):
+                blacklistURL = blacklistURL[blacklistURL.rfind(".", 0, blacklistURL.rfind("."))+1:len(blacklistURL)]#removes the subdomains
+            
+            blacklistFile = open("blacklist", "a")
+            blacklistFile.write("\n" + blacklistURL)#Write to disk
+            blacklistFile.close()
+            blacklist.append(blacklistURL)#Write to ram
+            await client.send_message(message.channel, blacklistURL + " has now been blacklisted")#feedback
+            print(blacklistURL + " has now been blacklisted by " + message.author.name + " " + message.author.id)
+        elif command[0:len('show')] == 'show':
+            await client.send_message(message.channel, "show")
+        else:
+            await client.send_message(message.channel, "Whats up?")
+        return
+
     if 'http://' in message.content:#for http links
         url = message.content[message.content.find('http://'):len(message.content)].split(' ')[0]
         if blacklisted(url) == False:
             await shorten(message, url)#sends the message object, and the url
+        return
 
     if 'https://' in message.content:#for https links
         url = message.content[message.content.find('https://'):len(message.content)].split(' ')[0]
         if blacklisted(url) == False:
             await shorten(message, url)
+        return
 
 @client.event
 async def on_ready():
@@ -58,6 +80,7 @@ def blacklisted(url = 'wesring.com'):
     rootUrl = rootUrl[rootUrl.rfind(".", 0, rootUrl.rfind("."))+1:len(rootUrl)]#removes the subdomains
     
     #print("Root: " + rootUrl)
+    print("Blacklist " + str(blacklist)) 
     for i in range(0,len(blacklist)):#check to see if its in the blacklist
         if blacklist[i].rstrip() == rootUrl:
             print("Blacklisted URL ignored: " + rootUrl)#url match
@@ -79,6 +102,9 @@ async def shorten(message, url = 'wesring.com'):#I am the error page
     print("done")
 
 print("Loading config")
+blacklistFile = open("blacklist", "r+")
 blacklist = blacklistFile.readlines()
+blacklistFile.close()
+config = configparser.ConfigParser()
 config.read("bot.config")
 client.run(config.get("bot","token"))
